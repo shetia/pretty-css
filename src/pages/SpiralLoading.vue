d78ac90b59c958fecbc12664c9186404966da144
unknown <wxj@DAD00001872.Infinitus.com.cn>
2020-05-07 17:44:21 +0800

    螺旋加载

---
+++ src/pages/SpiralLoading.vue
@@ -0,0 +1,117 @@
+<template>
+  <div  class="black-box">
+<div class="loading">
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+</div>
+  </div>
+</template>
+
+<script>
+export default {
+  pageName: '螺旋加载',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+
+.loading{
+  position: relative;
+  top: 50%;
+  left: 50%;
+}
+.circle {
+  $circle-count: 60;
+  $circle-diameter: 0.5em;
+  $ring-radius: 4em;
+  $duration: 3s;
+  position: absolute;
+  width: $circle-diameter;
+  height: $circle-diameter;
+  border-radius: 50%;
+  background: white;
+  opacity: 0;
+  animation: move-to-center $duration ease-in-out infinite;
+
+  @for $i from 1 through $circle-count {
+    &:nth-child(#{$i}) {
+      $ratio: $i / $circle-count;
+      // Make a ring of circles, and move them to center;
+      transform: rotate($ratio * 720deg) translateX($ring-radius);
+      // Then stagger the animation, and it becomes a spiral.
+      animation-delay: $ratio * $duration;
+    }
+  }
+}
+
+@keyframes move-to-center {
+  to {
+    opacity: 1;
+    transform: translate(0, 0);
+  }
+}
+
+</style>
