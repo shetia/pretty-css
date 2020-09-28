8a0ace3bc47069824c88e1147f9519ca684cc51a
unknown <wxj@DAD00001872.Infinitus.com.cn>
2020-09-28 16:44:43 +0800

    tab和双环

---
+++ src/pages/bicyclo.vue
@@ -0,0 +1,101 @@
+<template>
+  <div  class='black-box bicyclo'>
+    <div class=circle></div>
+    <div class=circle></div>
+    <svg>
+      <filter id="wave">
+        <feTurbulence x="0" y="0" baseFrequency="0.009" numOctaves="5" seed="2">
+          <animate attributeName="baseFrequercy" dur="60s" values="0.02; 0.05; 0.02" repeatCount="indefinite"></animate>
+        </feTurbulence>
+        <feDisplacementMap in="SourceGraphic" scale="60"></feDisplacementMap>
+      </filter>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  pageName: '双环',
+  data () {
+    return {
+
+    }
+  },
+  mounted () {
+
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+.bicyclo{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .circle{
+    position: relative;
+    width: 600px;
+    height: 600px;
+    filter: url(#wave) blur(1px);
+    &:nth-child(2)::before{
+      animation-delay: -2.5s;
+    }
+    &:before{
+      content:'';
+      position: absolute;
+      top: 100px;
+      left: 100px;
+      bottom: 100px;
+      right: 100px;
+      border:20px solid #fff;
+      border-radius: 100%;
+      box-shadow: 0px 0px 10px #fff, 0px 0px 20px #fff inset;
+    }
+    &:before{
+      content:'';
+      position: absolute;
+      top: 100px;
+      left: 100px;
+      bottom: 100px;
+      right: 100px;
+      border:20px solid #fff;
+      border-radius: 100%;
+      box-shadow: 0px 0px 50px #0f0, 0px 0px 50px #0f0 inset;
+      -webkit-box-reflect: below 20px linear-gradient(transparent, transparent, #0002);
+      animation: borderChange 5s linear infinite;
+      transition: all 10s;
+      @keyframes borderChange{
+        0%{
+          box-shadow: 0px 0px 50px #0f0, 0px 0px 50px #0f0 inset;
+          filter: hue-rotate(0deg);
+          transform: scale(1);
+        }
+        20%{
+          box-shadow: 0px 0px 60px #0f0, 0px 0px 60px #0f0 inset;
+        }
+        40%{
+          box-shadow: 0px 0px 40px #0f0, 0px 0px 40px #0f0 inset;
+        }
+        50%{
+          transform: scale(1.1);
+        }
+        60%{
+          box-shadow: 0px 0px 80px #0f0, 0px 0px 80px #0f0 inset;
+        }
+        80%{
+          box-shadow: 0px 0px 100px #0f0, 0px 0px 100px #0f0 inset;
+        }
+        100%{
+          box-shadow: 0px 0px 50px #0f0, 0px 0px 50px #0f0 inset;
+          filter: hue-rotate(360deg);
+          transform: scale(1);
+        }
+      }
+    }
+  }
+  svg{
+    width: 0px;
+    height: 0px;
+  }
+}
+</style>
---
+++ src/pages/tabs.vue
@@ -0,0 +1,140 @@
+<template>
+  <div  class='black-box tabs'>
+    <div class="tabs-left">
+      <div class="block-warp">
+        <div id="marker" :style="{'width': `${markerWidth}px`,'top': `${markerTop}px`}"></div>
+        <div class="warp-item">Home</div>
+        <div class="warp-item">About</div>
+        <div class="warp-item">Component</div>
+        <div class="warp-item">Methods</div>
+        <div class="warp-item">Contact</div>
+        <div class="warp-item">Link</div>
+      </div>
+    </div>
+    <div class="tabs-right">
+        <div class="block-warp">
+        <div id="marker2" :style="{'width': `${marker2Width}px`,'left': `${marker2Left}px`}"></div>
+        <div class="warp-item2">Home</div>
+        <div class="warp-item2">About</div>
+        <div class="warp-item2">Component</div>
+        <div class="warp-item2">Methods</div>
+        <div class="warp-item2">Contact</div>
+        <div class="warp-item2">Link</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  pageName: '导航tabs',
+  data () {
+    return {
+      markerWidth: 77,
+      markerTop: 0,
+      marker2Width: 77,
+      marker2Left: 0
+    }
+  },
+  mounted () {
+    this.marker()
+    this.marker2()
+  },
+  methods: {
+    marker () {
+      let items = document.querySelectorAll('.warp-item')
+      let indicator = (e) => {
+        this.markerWidth = e.offsetWidth
+        this.markerTop = e.offsetTop
+      }
+      items.forEach(item => {
+        item.addEventListener('mouseover', e => {
+          indicator(e.target)
+        })
+      })
+    },
+    marker2 () {
+      let items = document.querySelectorAll('.warp-item2')
+      let indicator = (e) => {
+        this.marker2Width = e.offsetWidth
+        this.marker2Left = e.offsetLeft
+      }
+      items.forEach(item => {
+        item.addEventListener('mouseover', e => {
+          indicator(e.target)
+        })
+      })
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+.tabs{
+  display:flex;
+  &-left{
+    width: 50%;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    .block-warp{
+      position:relative;
+      z-index: 9;
+      display:flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      .warp-item{
+        font-size: 24px;
+        line-height: 50px;
+        font-family: consolas;
+        cursor: pointer;
+        padding: 0px 12px;
+      }
+      #marker{
+        position: absolute;
+        top: 0px;
+        width: 77px;
+        z-index: -1;
+        height: 50px;
+        background:skyblue;
+        border-radius: 4px;
+        transition: all 0.2s;
+      }
+    }
+  }
+  &-right{
+    width: 50%;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    .block-warp{
+      position:relative;
+      z-index: 9;
+      display:flex;
+      justify-content: center;
+      align-items: center;    
+      .warp-item2{
+        font-size: 24px;
+        line-height: 50px;
+        font-family: consolas;
+        cursor: pointer;
+        padding: 0px 12px;
+      }
+      #marker2{
+        position: absolute;
+        left: 0px;
+        bottom: 0px;
+        z-index: -1;
+        width: 77px;
+        height: 5px;
+        background:rgb(165, 31, 98);
+        border-radius: 4px;
+        transition: all 0.25s;
+      }
+    }
+  }
+}
+</style>
